ef91397795251c04d730587c81e2ca45b01e50c6
NatnaelDemelash <natidemelash074@gmail.com>
2026-02-01 00:35:38 +0300

    Fix card click handler so cards actually flip when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Card } from "./components/Card";
 import { GameHeader } from "./components/GameHeader";
 
@@ -7,6 +8,49 @@ const cardValues = [
 ];
 
 export default function App() {
+
+  const [cards, setCards]  = useState([])
+
+  const intializeGame = () => {
+    // TODO:SHUFFLE CARDS
+
+    const finalCards = cardValues.map((value, index) => (
+      {
+        id: index,
+        value,
+        isFlipped: false,
+        isMatched: false
+      }
+    ));
+
+    setCards(finalCards);
+  }
+
+
+  useEffect(()=>{
+    intializeGame()
+  }, [])
+
+
+  const handleCardClick = (card) => {
+    // Donot allow card clic if the card is flipped or matched
+
+    if(card.isFlipped || card.isMatched){
+      return;
+    }
+
+    // update card flipped state
+    const newCard = cards.map((c) => {
+      if (c.id === card.id){
+        return {...c, isFlipped: true}
+      }else{
+        return c;
+      }
+    })
+
+    setCards(newCard)
+  }
+
   return (
     <div className="min-h-screen bg-gray-50 p-4">
       <div className="max-w-3xl mx-auto"> {/* Reduced max-width for 4×4 */}
@@ -14,8 +58,8 @@ export default function App() {
         
         {/* 4×4 Grid */}
         <div className="grid grid-cols-4 gap-3 md:gap-4 mt-8">
-          {cardValues.map((card, index) => (
-            <Card key={index} card={card} />
+          {cards.map((card, index) => (
+            <Card key={index} card={card} onClick={handleCardClick}/>
           ))}
         </div>
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,18 @@
-export const Card = ({ card }) => {
+export const Card = ({ card, onClick }) => {
     return (
-        <div className="aspect-square rounded-md bg-white shadow-xs hover:shadow-sm transition-shadow duration-200 flex items-center justify-center cursor-pointer">
-            <div className="text-6xl">
-                {card}
-            </div>
+        <div 
+            className="aspect-square rounded-md bg-white shadow-xs hover:shadow-sm transition-shadow duration-200 flex items-center justify-center cursor-pointer" 
+            onClick={() => onClick(card)}  
+        >
+            {!card.isFlipped ? (
+                <div className="text-6xl text-gray-300">  {/* Made text lighter */}
+                    ?
+                </div>
+            ) : (
+                <div className="text-6xl">
+                    {card.value}
+                </div>
+            )}
         </div>
     )
 }
